3c59a0451b897fa5aba7605029198f4849bf272a
osmedile <osmedile@859815d1-9555-0410-a025-737cb07e3660>
2008-09-24 15:10:20 +0000

    Better javadoc Removed dead code

--- src/main/java/osmedile/intellij/pasteall/PasteAllAction.java
+++ src/main/java/osmedile/intellij/pasteall/PasteAllAction.java
@@ -8,13 +8,22 @@ import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
 import com.intellij.openapi.ide.CopyPasteManager;
 
 /**
+ * Paste all items (limited by the mark) with older items first.
+ *
  * @author Olivier Smedile
  * @version $Id$
  */
 public class PasteAllAction extends EditorAction {
 
+    /**
+     * Return true if a new line character is added after each item pasted.
+     */
     private boolean newLine = true;
 
+    /**
+     * Limit the number of items to be pasted.
+     * <0, means that all items are pasted.
+     */
     private static int numberOfItems = -1;
 
 
@@ -41,22 +50,42 @@ public class PasteAllAction extends EditorAction {
         return newLine;
     }
 
+    /**
+     * Return true if the items must be pasted with older items first
+     *
+     * @return
+     */
     public boolean getOlderFirst() {
         return true;
     }
 
+    /**
+     * Set the mark, so only items copied from now will be pasted
+     */
     public static void setMark() {
         numberOfItems = 0;
     }
 
+    /**
+     * Cancel mark -> all items will be pasted
+     */
     public static void cancelMark() {
         numberOfItems = -1;
     }
 
+    /**
+     * Increment mark, one more item can be pasted
+     */
     public static void incNumberOfItems() {
         numberOfItems++;
     }
 
+    /**
+     * Returns the number of items to be pasted.
+     * <0, means that all items must be pasted
+     *
+     * @return
+     */
     public static int getNumberOfItems() {
         return numberOfItems;
     }
