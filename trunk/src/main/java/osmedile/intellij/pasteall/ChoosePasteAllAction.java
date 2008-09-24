b1b376a965ae63c575e558f9c7b2d08ed8c67445
osmedile <osmedile@859815d1-9555-0410-a025-737cb07e3660>
2008-09-24 15:34:05 +0000

    Better javadoc Focus is now set to the list of items Updated plugin change notes

--- trunk/src/main/java/osmedile/intellij/pasteall/ChoosePasteAllAction.java
+++ trunk/src/main/java/osmedile/intellij/pasteall/ChoosePasteAllAction.java
@@ -6,14 +6,31 @@ import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler;
 
 /**
+ * Show a dialog to paste multiple items.
+ *
  * @author Olivier Smedile
  * @version $Id$
  */
 public class ChoosePasteAllAction extends EditorAction {
 
+    /**
+     * Used to remember "use template" checkbox status.
+     */
     private boolean useTemplate = false;
+
+    /**
+     * Used to remember the selected template in Combo box of available templates.
+     */
     private int tplIdx = 0;
+
+    /**
+     * Used to remember the template.
+     */
     private String template = "";
+
+    /**
+     * Used to remember if items are pasted with older first.
+     */
     private boolean olderFirst = true;
 
     public ChoosePasteAllAction() {
@@ -22,12 +39,12 @@ public class ChoosePasteAllAction extends EditorAction {
         this.setupHandler(new EditorWriteActionHandler() {
             public void executeWriteAction(Editor editor, DataContext dc) {
 
+                //reapply saved configuration of dialog
                 ChooseContentUI chooseContentUI =
                         new ChooseContentUI(editor.getProject(), editor);
                 chooseContentUI.getUseTemplatChk().setSelected(useTemplate);
                 chooseContentUI.getTemplateBox().setSelectedIndex(tplIdx);
-                chooseContentUI.getTemplateViewer().getDocument()
-                        .setText(template);
+                chooseContentUI.getTemplateViewer().getDocument().setText(template);
                 if (olderFirst) {
                     chooseContentUI.getOlderFirst().setSelected(true);
                 } else {
@@ -37,12 +54,14 @@ public class ChoosePasteAllAction extends EditorAction {
 
                 chooseContentUI.pack();
                 chooseContentUI.setLocationByPlatform(true);
+                chooseContentUI.getTransList().requestFocus();
+                chooseContentUI.getTransList().requestFocusInWindow();
                 chooseContentUI.setVisible(true);
 
+                //save dialog configuration
                 useTemplate = chooseContentUI.getUseTemplatChk().isSelected();
                 tplIdx = chooseContentUI.getTemplateBox().getSelectedIndex();
-                template = chooseContentUI.getTemplateViewer().getDocument()
-                        .getText();
+                template = chooseContentUI.getTemplateViewer().getDocument().getText();
                 olderFirst = chooseContentUI.getOlderFirst().isSelected();
 
             }
